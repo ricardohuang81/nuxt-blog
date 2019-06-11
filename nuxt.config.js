971697004406615413688d3ca559dd3e5bbebe51
Ricky Huang <huangricky@gmail.com>
2019-06-11 00:30:03 -0400

    minor change to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,9 +82,9 @@ export default {
         .then(res => {
           const routes = []
           for (const key in res.data) {
-            routes.push({ 
-              route: '/posts/' + key, 
-              payload: { postData: res.data[key] } 
+            routes.push({
+              route: '/posts/' + key,
+              payload: { postData: res.data[key] }
             })
           }
           return routes
